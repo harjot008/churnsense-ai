9360c159538215c5dec75b3593a88b4936a2a5b3
Harjot Singh <harjotbhogal2012@gmail.com>
2026-07-22 21:00:16 +0530

    Each input data field inside form

--- app.py
+++ app.py
@@ -42,122 +42,126 @@ st.sidebar.title("Input values")
     # Assumptions:
     #     As an MVP we do not allow None values passed to a column
 
-# Gender
-gender = st.sidebar.selectbox(
-    "Select gender",
-    ["Male", "Female"]
-)
-
-st.write("Selected gender:", gender)
-
-# Senior Citizen
-senior_citizen = st.sidebar.selectbox(
-    "Are you senior citizen?",
-    ["Yes", "No"]
-)
-
-senior_citizen = 1 if senior_citizen == "Yes" else 0
-
-# Partner
-partner = st.sidebar.selectbox(
-    "Do have have any partner?",
-    ["Yes", "No"]
-)
-
-# Dependents column
-dependents = st.sidebar.selectbox(
-        "Do you have dependents?",
-        ["No", "Yes"]
-)
-
-# tenure column
-tenure = st.sidebar.slider(
-        "Tenure in months",
-        min_value=0,
-        max_value=100,
-        value=1,
-        step=1
-)
-
-# PhoneService column
-phone_service = st.sidebar.selectbox(
-        "Phone service",
-        ["No", "Yes"]
-)
-
-multiple_lines = st.sidebar.selectbox(
-        "Multiple lines",
-        ["No phone service", "No", "Yes"]
+with st.sidebar.form("customer_form"):
+    # Gender
+    gender = st.selectbox(
+        "Select gender",
+        ["Male", "Female"]
     )
 
-internet_service = st.sidebar.selectbox(
-        "Internet service",
-        ["DSL", "Fiber optic", "No"]
+    # Senior Citizen
+    senior_citizen = st.selectbox(
+        "Are you senior citizen?",
+        ["Yes", "No"]
     )
 
-online_security = st.sidebar.selectbox(
-    "Online security",
-    ["No", "Yes", "No internet service"]
-)
+    senior_citizen = 1 if senior_citizen == "Yes" else 0
 
-online_backup = st.sidebar.selectbox(
-    "Online backup",
-    ["Yes", "No", "No internet service"]
-)
+    # Partner
+    partner = st.selectbox(
+        "Do have have any partner?",
+        ["Yes", "No"]
+    )
 
-device_protection = st.sidebar.selectbox(
-    "Device protection",
-    ["No", "Yes", "No internet service"]
-)
+    # Dependents column
+    dependents = st.selectbox(
+            "Do you have dependents?",
+            ["No", "Yes"]
+    )
 
-tech_support = st.sidebar.selectbox(
-    "Tech support",
-    ["No", "Yes", "No internet service"]
-)
+    # tenure column
+    tenure = st.slider(
+            "Tenure in months",
+            min_value=0,
+            max_value=100,
+            value=1,
+            step=1
+    )
 
-streaming_tv = st.sidebar.selectbox(
-    "Streaming TV",
-    ["No", "Yes", "No internet service"]
-)
+    # PhoneService column
+    phone_service = st.selectbox(
+            "Phone service",
+            ["No", "Yes"]
+    )
 
-streaming_movies = st.sidebar.selectbox(
-    "Streaming movies",
-    ["No", "Yes", "No internet service"]
-)
+    multiple_lines = st.selectbox(
+            "Multiple lines",
+            ["No phone service", "No", "Yes"]
+        )
 
-contract = st.sidebar.selectbox(
-    "Contract",
-    ["Month-to-month", "One year", "Two year"]
-)
+    internet_service = st.selectbox(
+            "Internet service",
+            ["DSL", "Fiber optic", "No"]
+        )
 
-paperless_billing = st.sidebar.selectbox(
-    "Paperless billing",
-    ["Yes", "No"]
-)
+    online_security = st.selectbox(
+        "Online security",
+        ["No", "Yes", "No internet service"]
+    )
 
-payment_method = st.sidebar.selectbox(
-    "Payment method",
-    [
-        "Electronic check",
-        "Mailed check",
-        "Bank transfer (automatic)",
-        "Credit card (automatic)"
-    ]
-)
+    online_backup = st.selectbox(
+        "Online backup",
+        ["Yes", "No", "No internet service"]
+    )
 
-# monthly charges and total charges
-monthly_charges = st.sidebar.number_input(
-    "Monthly charges",
-    min_value=0.0,
-    max_value=150.0,
-    value=20.0,
-    step=0.1
-)
+    device_protection = st.selectbox(
+        "Device protection",
+        ["No", "Yes", "No internet service"]
+    )
 
-total_charges = st.sidebar.number_input(
-    "Total charges",
-    min_value=0.0,
-    max_value=10000.0,
-    value=20.0,
-    step=0.1
-)
+    tech_support = st.selectbox(
+        "Tech support",
+        ["No", "Yes", "No internet service"]
+    )
+
+    streaming_tv = st.selectbox(
+        "Streaming TV",
+        ["No", "Yes", "No internet service"]
+    )
+
+    streaming_movies = st.selectbox(
+        "Streaming movies",
+        ["No", "Yes", "No internet service"]
+    )
+
+    contract = st.selectbox(
+        "Contract",
+        ["Month-to-month", "One year", "Two year"]
+    )
+
+    paperless_billing = st.selectbox(
+        "Paperless billing",
+        ["Yes", "No"]
+    )
+
+    payment_method = st.selectbox(
+        "Payment method",
+        [
+            "Electronic check",
+            "Mailed check",
+            "Bank transfer (automatic)",
+            "Credit card (automatic)"
+        ]
+    )
+
+    # monthly charges and total charges
+    monthly_charges = st.number_input(
+        "Monthly charges",
+        min_value=0.0,
+        max_value=150.0,
+        value=20.0,
+        step=0.1
+    )
+
+    total_charges = st.number_input(
+        "Total charges",
+        min_value=0.0,
+        max_value=10000.0,
+        value=20.0,
+        step=0.1
+    )
+
+    submitted = st.form_submit_button("Submit")
+
+if submitted:
+    st.sidebar.write("Details submitted")
